cef06fe0735a241f20a231bcc67193379bb4d957
urmi1506 <urmilaawasarmal@gmail.com>
2024-12-16 17:13:18 +0530

    Demo of Variables & Typecast

--- Basics/demo.py
+++ Basics/demo.py
@@ -1,1 +1,26 @@
 print("Hii There...")
+
+# variables
+a=25
+print (a)
+
+b="Urmi"
+print (b)
+
+c=True
+print (c)
+
+d=True + True  #it will store in 0 1 format
+print (d)
+
+e=1 - False
+print (e)
+
+
+x=6+1j  #j represent imaginery no
+print (x.real)
+print (x.imag)
+
+ans=input()   #i/p always print everything in string format
+res=a+int (ans)  #Typecasting
+print (res)
