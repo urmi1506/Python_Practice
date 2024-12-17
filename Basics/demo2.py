9eae823b738bd91e88ab3aab980a41b84533c365
urmi1506 <urmilaawasarmal@gmail.com>
2024-12-17 21:28:24 +0530

    tuples and list

---
+++ Basics/demo2.py
@@ -0,0 +1,18 @@
+# Tuples
+t1=(1,"urmi",3+1j,4.55)
+# access data
+print(t1[0:2])
+# reverse order
+print(t1[::-1])
+# type
+print(type(t1))
+
+# List
+l=[1,"urmi",3+1j,4.55]
+# access data
+print(l[0:2])
+# reverse order
+print(l[::-1])
+# type
+print(type(l))
+
