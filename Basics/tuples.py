5ab18435c0c1dde2ff1a1e7688e5ccd762d1e2e9
urmi1506 <urmilaawasarmal@gmail.com>
2024-12-21 00:09:45 +0530

    Tuple & its operation

--- Basics/tuples.py
+++ Basics/tuples.py
@@ -6,3 +6,15 @@ print(t1*2)
 print(t1.count('u'))
 # its not work for specific character
 print(t1.index('urmi'))
+
+# nested tuples
+t3=('hey',2,('hello',45))
+print(t3)
+
+t4=([1,2,'hii'],('hii there',3.5))
+print(t4)
+print(t4[0][2])
+
+t4[0][2]='hello'
+print(t4)
+
