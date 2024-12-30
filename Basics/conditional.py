c2256960b1381fda3cddf872ac9092a6ff8f7f5e
urmi1506 <urmilaawasarmal@gmail.com>
2024-12-30 23:54:47 +0530

    all conditional statements

--- Basics/conditional.py
+++ Basics/conditional.py
@@ -3,7 +3,7 @@ if a < 15 :
     print("Hii There..")
 else:
     print("Its not satisfied condition")
-
+# ----- #
 income=int(input())
 if income< 50:
     print("Able to Buy phone")
@@ -13,7 +13,7 @@ elif income < 90:
     print("Able to buy home")
 else:
     print("not able to buy anything")
-
+# ----- #
 Total_price=int(input())
 if Total_price >2000:
     discount= Total_price * .20
@@ -23,3 +23,23 @@ elif Total_price > 5000 :
     print("discount will be :",discount)
 else:
     print("No Discount")
+# ----- #
+coup=input()
+if coup == 'new5' :
+  print("You able to get discount")
+  paid_amt=7000 - 7000 * 0.5
+  print("Total paid amount ",paid_amt)
+else:
+   print("Valid coupon code")
+# ----- #
+Study_hr=int(input())
+if Study_hr >1:
+    print("Take 9-10 month to progress")
+elif Study_hr > 2 and Study_hr < 5 :
+    print("Take 4-5 month to progress")
+else:
+    print("No Progress")
+# ----- #
+
+
+
---
+++ Basics/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+Study_hr=int(input())
+if Study_hr >1:
+    print("Take 9-10 month to progress")
+elif Study_hr > 2 and Study_hr < 5 :
+    print("Take 4-5 month to progress")
+else:
+    print("No Progress")
