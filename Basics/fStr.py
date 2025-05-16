99c155381c111875aa53defcb26cf4801ed21681
urmi1506 <urmilaawasarmal@gmail.com>
2025-05-16 20:28:03 +0530

    basic python

---
+++ Basics/fStr.py
@@ -0,0 +1,16 @@
+food = "White Pasta"
+print(f"i love {food}")
+
+user_name = "Urmi"
+year = 2025
+pi = 3.14
+is_admin = True
+
+print(f"The User name is :{user_name}")
+print(f"The Year is :{year}")
+print(f"The Value of pie is :{pi}")
+
+if is_admin:
+    print("You are Admin")
+else:
+    print("Anonymous Person")
