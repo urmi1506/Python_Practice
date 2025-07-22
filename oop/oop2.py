51a81276f088c17bcb05f850c27123ca16a2dc2f
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-22 19:03:54 +0530

    OOP-Basics

---
+++ oop/oop2.py
@@ -0,0 +1,11 @@
+class Emp:
+    def __init__(self):
+        self.age=22
+        self.name="urmi"
+    def show(self):
+        print(self.age,self.name)
+e=Emp()
+e.salary=1000000.00
+e.show()
+# print("Age:", e.age ,"Name:",e.name,"Salary:",e.salary)
+
