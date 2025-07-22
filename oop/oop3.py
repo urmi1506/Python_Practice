7da31de1b341a82ce30b6933c255ec6999209e7a
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-22 19:13:18 +0530

    oop-basics

---
+++ oop/oop3.py
@@ -0,0 +1,10 @@
+class Emp:
+    def __init__(self,age,name,salary):
+         self.age=age
+         self.name=name
+         self.salary=salary
+
+e=Emp(21,"sari",35000)
+print("Name:",e.name,"Age:",e.age,"Salary",e.salary)
+e2=Emp(20,"Prema",40000)
+print("Name:",e2.name,"Age:",e2.age,"Salary:",e2.salary)
